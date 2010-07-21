2c4601569685a14c9ff3fb25328c2ce86e83b9a2
Jeremy Corbett <jeremy.lael@gmail.com>
2010-07-21 12:33:05 -0400

    m2n2 added

--- prep.c
+++ prep.c
@@ -4,12 +4,17 @@
 
 extern void m2n1();
 
+extern void m2n2();
+
 void pertubations(int m,int n,double real,double imag,double xinit,double yinit,double deltax,double deltay,int x,int y,char *array){
 	switch(m){
 		case 2:
 			switch(n){
 				case 1:
 					m2n1(real,imag,xinit,yinit,deltax,deltay,x,y,array);
+					break;
+				case 2:
+					m2n2(real,imag,xinit,yinit,deltax,deltay,x,y,array);
 					break;
 			}
 			break;
